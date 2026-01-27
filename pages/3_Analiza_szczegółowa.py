bf9da92410cb09e5fd5da795fc4dae348b0bdd9c
admtr <aleksandra.dmitruk06@gmail.com>
2026-01-27 22:22:36 +0100

    🎨 Improved structure of the code

--- pages/3_Analiza_szczegółowa.py
+++ pages/3_Analiza_szczegółowa.py
@@ -4,7 +4,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-
 from wordcloud import WordCloud
 
 st.set_page_config(page_title="Sleep, Mood & Messages", layout="wide")
